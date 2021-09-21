226a02852be5b231bdd06fe9f549aab0913baf7b
Evans Ding <evansding@gmail.com>
2021-09-21 15:22:35 -0700

    update carasoul

---
+++ app/src/Carasoul.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import play from './pics/play.png';
+import share from './pics/share.png';
+import download from './pics/download.png';
+
+
+
+const Image = styled.img`
+  width: 100px;
+  border-radius: 5px;
+
+`;
+
+const SmallImage = styled.img`
+  width: 25%;
+  color: grey;
+  overflow: hidden;
+`;
+
+
+
+
+const PlayButton = styled.img`
+  position: absolute;
+  z-index: 2;
+  opacity: 0.7;
+  margin-top: 15%;
+`;
+
+const InteractContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  border-radius: 25px;
+  >a {
+    width: 100%;
+    border-style: solid;
+    border-color: grey;
+    border-width: thin;
+  }
+`;
+
+
+
+
+
+const Carasoul = (props) => {
+
+  const {cover_photo_url, download_url, media_type, tracking_link} = props.media;
+
+
+  return(
+    <div>
+      <Image src = {cover_photo_url} alt = 'coverphoto'></Image>
+      {media_type === 'video' && <PlayButton src = {play} alt = 'playbutton'></PlayButton>}
+      <InteractContainer>
+        <a href = {tracking_link}>
+          <SmallImage src = {share} alt = 'share'></SmallImage>
+        </a>
+        <a href = {download_url}>
+          <SmallImage src = {download} alt = 'share'></SmallImage>
+        </a>
+      </InteractContainer>
+    </div>
+  )
+}
+
+export default Carasoul;
--- app/src/Module.jsx
+++ app/src/Module.jsx
@@ -1,6 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import IconName from './IconName.jsx';
+import Carasoul from './Carasoul.jsx';
+
+import styled, { css } from 'styled-components';
+
 //import { Text, View } from 'react-native';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  overflow: hidden;
+  align-items: baseline;
+  justify-content: space-between;
+`;
 
 const Module = (props) => {
 
@@ -8,6 +20,17 @@ const Module = (props) => {
   return(
     <div>
       <IconName info = {props.info}/>
+      <Container>
+        {props.info.medias.map((item, index) => {
+          if(index < 5) {
+            return(
+              <Carasoul media = {item} key = {index}/>
+            )
+          }
+
+        })}
+      </Container>
+
     </div>
   )
 }
